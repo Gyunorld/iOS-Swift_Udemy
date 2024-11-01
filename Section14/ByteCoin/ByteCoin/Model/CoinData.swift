3edea22c6a5b71c9a6d3d3ea827e57bc0e6fe386
kimmingyu <mingue640@gmail.com>
2024-11-01 15:15:32 +0900

    section14

---
+++ Section14/ByteCoin/ByteCoin/Model/CoinData.swift
@@ -0,0 +1,13 @@
+//
+//  CoinData.swift
+//  ByteCoin
+//
+//  Created by 김민규 on 11/1/24.
+//  Copyright © 2024 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct CoinData: Codable {
+    let rate: Double
+}
